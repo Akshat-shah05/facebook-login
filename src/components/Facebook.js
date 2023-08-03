169ef869802cb8d081febe17f768495d6e2812f1
Akshat-shah05 <akshatshah1605@gmail.com>
2023-08-03 13:24:45 -0400

    Log in authentication logic added!

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -1,45 +1,59 @@
-import React, { Component } from 'react';
-import { FacebookLogin } from 'react-facebook-login';
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
 
 export default class Facebook extends Component {
-
   state = {
     isLoggedIn: false,
-    userID: '',
-    name: '',
-    email: '',
-    picture: ''
-  }
+    userID: "",
+    name: "",
+    email: "",
+    picture: ""
+  };
 
   responseFacebook = response => {
-    console.log(response)
-  }
+    // console.log(response);
 
-  componentClicked = () => {
-    console.log("clicked")
-  }
+    this.setState({
+      isLoggedIn: true,
+      userID: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url
+    });
+  };
+
+  componentClicked = () => console.log("clicked");
 
   render() {
-
     let fbContent;
 
     if (this.state.isLoggedIn) {
-
+      fbContent = (
+        <div
+          style={{
+            width: "400px",
+            margin: "auto",
+            background: "#f4f4f4",
+            padding: "20px"
+          }}
+        >
+          <img src={this.state.picture} alt={this.state.name} />
+          <h2>Welcome {this.state.name}</h2>
+          Email: {this.state.email}
+        </div>
+      );
     } else {
-        fbContent = (
-            <FacebookLogin
-            appId="979944183336297"
-            autoLoad={true}
-            fields="name,email,picture"
-            onClick={this.componentClicked}
-            callback={this.responseFacebook} />
-        );
+      fbContent = (
+        <FacebookLogin
+          appId="189486938370592"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
     }
 
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div>{fbContent}</div>;
   }
 }
